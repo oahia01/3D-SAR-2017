88ebd64183284c9eb7392e2d5e6291ad750a920c
Ethan Chan <ethan.chan@tufts.edu>
2017-02-16 22:50:47 -0500

    changed README.md

--- 3d_autofocus/grad_h.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdlib>
-
-const double delta = 1e-3;
-
-// TODO: Nice doc comments
-void gradH(double *phi_offsets, const double *Br, const double *Bi,
-        double *grad, std::size_t K, std::size_t B_len);
